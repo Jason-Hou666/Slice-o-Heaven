abda5eea80108d1888333a23213b10c91bc2b6bb
Jason-Hou666 <781215041@qq.com>
2025-02-23 19:36:05 +0800

    Slice-o-Heaven

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        LuckinCoffee luckcoffee = new LuckinCoffee();
-        luckcoffee.takeOrder("1234", "2 lattes", 20.00);
-        
-    }
-}
+    SliceoHeaven sliceoheaven = new SliceoHeaven();
+    sliceoheaven.takeOrder("1234", "2 pizza", 20.00);
+ 
+     }
+    } 
